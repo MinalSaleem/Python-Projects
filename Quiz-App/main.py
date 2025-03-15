fad17b2b1bef9792403811301a2c3514bf9e97f1
MinalSaleem <minalsaleem9@gmail.com>
2025-03-16 04:06:01 +0500

    quiz app by streamlit

---
+++ Quiz-App/main.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import random
+import time
+
+st.title("📃 Quiz Application")
+
+questions = [
+    {
+        "question": "What is the capital of Pakistan?",
+        "options": ["Lahore", "Karachi", "Islamabad", "Peshawar"],
+        "answer": "Islamabad",
+    },
+    {
+        "question": "Who is the founder of Pakistan?",
+        "options": [
+            "Allama Iqbal",
+            "Liaquat Ali Khan",
+            "Muhammad Ali Jinnah",
+            "Benazir Bhutto",
+        ],
+        "answer": "Muhammad Ali Jinnah",
+    },
+    {
+        "question": "Which is the national language of Pakistan?",
+        "options": ["Punjabi", "Urdu", "Sindhi", "Pashto"],
+        "answer": "Urdu",
+    },
+    {
+        "question": "What is the currency of Pakistan?",
+        "options": ["Rupee", "Dollar", "Taka", "Riyal"],
+        "answer": "Rupee",
+    },
+    {
+        "question": "Which city is known as the City of Lights in Pakistan?",
+        "options": ["Lahore", "Islamabad", "Faisalabad", "Karachi"],
+        "answer": " Karachi",
+    },
+]
+
+
+if "current_question" not in st.session_state:
+    st.session_state.current_question = random.choice(questions)
+
+question = st.session_state.current_question
+
+st.subheader(question["question"])
+
+selected_option = st.radio("Choose the answer:", question["options"], key="answer")
+
+if st.button("Submit Answer"):
+    if selected_option == question["answer"]:
+        st.success("✅ Correct!")
+    else:
+        st.error("❌ Incorrect! the correct answer is "+ question["answer"])
+        
+        time.sleep(3)
+        
+        st.session_state.current_quesyion = random.choice(questions)
+
+        st.rerun()
